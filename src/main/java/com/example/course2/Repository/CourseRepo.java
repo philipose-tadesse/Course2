c38e5d3e63475bc08ae006c9b2787ec16abceb50
philipose <philipose.tadesse@gmail.com>
2023-08-25 15:00:24 -0500

    some change on courseRepo class

--- src/main/java/com/example/course2/Repository/CourseRepo.java
+++ src/main/java/com/example/course2/Repository/CourseRepo.java
@@ -1,5 +1,6 @@
 package com.example.course2.Repository;
 
+import com.example.course2.binding.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
